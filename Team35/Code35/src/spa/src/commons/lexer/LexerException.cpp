715e6c1199fcf8e710762373d9a010e35b52ad8a
Ng Jun Kang <ng_jun_kang@hotmail.com>
2023-02-04 22:19:12 +0800

    fix dangling pointer problem

--- Team35/Code35/src/spa/src/commons/lexer/LexerException.cpp
+++ Team35/Code35/src/spa/src/commons/lexer/LexerException.cpp
@@ -4,10 +4,12 @@
 #include <utility>
 
 LexerException::LexerException(std::string message, LineNumber line_number) : message_(std::move(message)),
-                                                                              line_number_(line_number) {}
-
-const char *LexerException::what() const noexcept {
+                                                                              line_number_(line_number) {
     std::stringstream s;
     s << "line " << line_number_ << ": " << "lexical error: " << message_;
-    return s.str().c_str();
+    message_ = s.str();
+}
+
+const char *LexerException::what() const noexcept {
+    return message_.c_str();
 }
