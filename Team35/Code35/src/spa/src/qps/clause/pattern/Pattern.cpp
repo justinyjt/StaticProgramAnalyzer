52348adcc2668902029d85c277958e62dfaea065
Ng Jun Kang <ng_jun_kang@hotmail.com>
2023-02-06 19:43:34 +0800

    fix styles

--- Team35/Code35/src/spa/src/qps/clause/pattern/Pattern.cpp
+++ Team35/Code35/src/spa/src/qps/clause/pattern/Pattern.cpp
@@ -5,14 +5,14 @@ Pattern::Pattern(Tok first, Tok second) : Clause(first, second) {}
 
 // select a pattern a ( _ , "x + 1")
 
-Result* Pattern::evaluate(PKBReader* db) {
-  // if first == "_" and second is string with no underscores
-  if (first.tag == Tok::WILDCARD && second.tag == Tok::EXPR) {
-    std::string pattern = second.getValue();
-    STMT_SET set = db->getStmtWithExactPatternMatch(pattern);
-    // temporary workaround
-    IntResult* result = new IntResult(set);
-    return dynamic_cast<Result*>(result);
-  }
-  throw std::runtime_error("");
+Result *Pattern::evaluate(PKBReader *db) {
+    // if first == "_" and second is string with no underscores
+    if (first.tag == Tok::WILDCARD && second.tag == Tok::EXPR) {
+        std::string pattern = second.getValue();
+        STMT_SET set = db->getStmtWithExactPatternMatch(pattern);
+
+        IntResult *result = new IntResult(set);
+        return dynamic_cast<Result *>(result);
+    }
+    throw std::runtime_error("");
 }
--- Team35/Code35/src/spa/src/qps/clause/relationship/Modify.cpp
+++ Team35/Code35/src/spa/src/qps/clause/relationship/Modify.cpp
@@ -5,16 +5,16 @@ Modify::Modify(Tok first, Tok second) : Relationship(first, second) {}
 
 // select v such that modifies(1, v)
 
-Result* Modify::evaluate(PKBReader* db) {
-  if (first.tag == Tok::STMT_NUM && second.tag == Tok::SYNONYM) {
-    ENT_SET set = db->getRelationship(StmtNameRelationship::Modifies,
-                                      std::stoi(first.getValue()));
-    // temporary workaround
-    StrResult* result = new StrResult(set);
-    return dynamic_cast<Result*>(result);
-  }
+Result *Modify::evaluate(PKBReader *db) {
+    if (first.tag == Tok::STMT_NUM && second.tag == Tok::SYNONYM) {
+        ENT_SET set = db->getRelationship(StmtNameRelationship::Modifies,
+                                          std::stoi(first.getValue()));
 
-  throw std::runtime_error("");
+        StrResult *result = new StrResult(set);
+        return dynamic_cast<Result *>(result);
+    }
+
+    throw std::runtime_error("");
 }
 
 bool Modify::operator==(const Modify &rhs) const {
