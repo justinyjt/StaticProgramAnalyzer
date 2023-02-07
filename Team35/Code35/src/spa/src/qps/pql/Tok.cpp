6dd82cb74ab220e65948fa93767b95bf98f11644
nguyiyang <nguyiyang47@gmail.com>
2023-02-07 10:58:50 +0800

    Update Tok equals method

--- Team35/Code35/src/spa/src/qps/pql/Tok.cpp
+++ Team35/Code35/src/spa/src/qps/pql/Tok.cpp
@@ -3,7 +3,7 @@
 Tok::Tok(std::string value, Tag tag) : value(value), tag(tag) {}
 
 bool Tok::operator==(const Tok &rhs) const {
-    return this->tag == rhs.tag;
+    return this->tag == rhs.tag && this->value == rhs.value;
 }
 
 std::string Tok::getValue() {
