316cb8c96d0c5a67507c9dd74786d5f79df6e35c
Dave Low <davelowqx@gmail.com>
2023-02-16 04:03:48 +0800

    remove unnecessary dynamic casts

--- Team35/Code35/src/spa/src/qps/clause/relationship/Modifies.cpp
+++ Team35/Code35/src/spa/src/qps/clause/relationship/Modifies.cpp
@@ -9,10 +9,10 @@ Result* ModifiesS::evaluate(PKBReader *db) {
 
     switch (caseValue()) {
         case c(PQLToken::Tag::STMT_NUM, PQLToken::Tag::SYNONYM):  // Modifies(1, var) -> string[]
-        {            int stmtNum = (dynamic_cast<const StatementNumber*>(first))->n;
-            std::string synonymIdent = (dynamic_cast<const Synonym*>(second))->ident;
+        {            
+            int stmtNum = (dynamic_cast<const StatementNumber*>(first))->n;
             ENT_SET set = db->getRelationship(StmtNameRelationship::Modifies, stmtNum);
-            Result *result = new OneColResult<std::string>(synonymIdent, set);
+            Result *result = new OneColResult<std::string>(second->str(), set);
             return result;
         }
         case c(PQLToken::Tag::STMT_NUM, PQLToken::Tag::WILDCARD):  // Modifies(1, _) -> bool
@@ -25,35 +25,32 @@ Result* ModifiesS::evaluate(PKBReader *db) {
         case c(PQLToken::Tag::STMT_NUM, PQLToken::Tag::IDENT):  // Modifies(1, "x") -> bool
         {
             int stmtNum = (dynamic_cast<const StatementNumber*>(first))->n;
-            bool b = db->isRelationshipExists(StmtNameRelationship::Modifies, stmtNum,
-                                        (dynamic_cast<const Ident*>(second))->str());
+            bool b = db->isRelationshipExists(StmtNameRelationship::Modifies, 
+                                            stmtNum, second->str());
             Result *result = new BoolResult(b);
             return result;
         }
         case c(PQLToken::Tag::SYNONYM, PQLToken::Tag::SYNONYM):  // Modifies(stmt, var) -> pair<int, string>[]
         {
-            std::string synonymIdent = (dynamic_cast<const Synonym*>(first))->ident;
             std::vector<std::pair<int, std::string>> vec;
             STMT_ENT_SET set = db->getAllRelationships(StmtNameRelationship::Modifies);
             for (auto& se : set)                vec.push_back(se);
-            Result *result = new TwoColResult<int, std::string>(first->str(), synonymIdent, vec);
+            Result *result = new TwoColResult<int, std::string>(first->str(), second->str(), vec);
             return result;
         }
         case c(PQLToken::Tag::SYNONYM, PQLToken::Tag::WILDCARD):  // Modifies(stmt, _) -> int[]
         {
-            std::string synonymIdent = (dynamic_cast<const Synonym*>(first))->ident;
             STMT_ENT_SET set = db->getAllRelationships(StmtNameRelationship::Modifies);
             std::vector<int> vec;
             for (const STMT_ENT& se : set)
                 vec.push_back(se.first);
-            Result *result = new OneColResult<int>(synonymIdent, vec);
+            Result *result = new OneColResult<int>(first->str(), vec);
             return result;
         }
         case c(PQLToken::Tag::SYNONYM, PQLToken::Tag::IDENT):  // Modifies(stmt, "x")/ -> int[]
         {
-            std::string synonymIdent = (dynamic_cast<const Synonym*>(first))->ident;
             STMT_SET stmtSet = db->getRelationship(StmtNameRelationship::Modifies, second->str());
-            Result *result = new OneColResult<int>(synonymIdent, stmtSet);
+            Result *result = new OneColResult<int>(first->str(), stmtSet);
             return result;
         }
         default:
--- Team35/Code35/src/spa/src/qps/clause/relationship/Uses.cpp
+++ Team35/Code35/src/spa/src/qps/clause/relationship/Uses.cpp
@@ -10,10 +10,8 @@ Result* UsesS::evaluate(PKBReader *db) {
         case c(PQLToken::Tag::STMT_NUM, PQLToken::Tag::SYNONYM):  // Uses(1, var) -> string[]
         {
             int stmtNum = (dynamic_cast<const StatementNumber*>(first))->n;
-
-            std::string synonymIdent = (dynamic_cast<const Synonym*>(second))->ident;
             ENT_SET set = db->getRelationship(StmtNameRelationship::Uses, stmtNum);
-            Result *result = new OneColResult<std::string>(synonymIdent, set);
+            Result *result = new OneColResult<std::string>(second->str(), set);
             return result;
         }
         case c(PQLToken::Tag::STMT_NUM, PQLToken::Tag::WILDCARD):  // Uses(1, _)/ -> bool
@@ -26,37 +24,34 @@ Result* UsesS::evaluate(PKBReader *db) {
         case c(PQLToken::Tag::STMT_NUM, PQLToken::Tag::IDENT):  // Uses(1, "x")/ -> bool
         {
             int stmtNum = (dynamic_cast<const StatementNumber*>(first))->n;
-            bool b = db->isRelationshipExists(StmtNameRelationship::Uses, stmtNum,
-                                        (dynamic_cast<const Ident*>(second))->str());
+            bool b = db->isRelationshipExists(StmtNameRelationship::Uses, stmtNum, 
+                                            second->str());
             Result *result = new BoolResult(b);
             return result;
         }
         case c(PQLToken::Tag::SYNONYM, PQLToken::Tag::SYNONYM):  // Uses(stmt, var)/ -> pair<int, string>[]
         {
-            std::string synonymIdent = (dynamic_cast<const Synonym*>(first))->ident;
             std::vector<std::pair<int, std::string>> vec;
             STMT_ENT_SET set = db->getAllRelationships(StmtNameRelationship::Uses);
             for (auto& se : set) {
                 vec.push_back(se);
             }
-            Result *result = new TwoColResult<int, std::string>(first->str(), synonymIdent, vec);
+            Result *result = new TwoColResult<int, std::string>(first->str(), second->str(), vec);
             return result;
         }
         case c(PQLToken::Tag::SYNONYM, PQLToken::Tag::WILDCARD):  // Uses(stmt, _)/ -> int[]
         {
-            std::string synonymIdent = (dynamic_cast<const Synonym*>(first))->ident;
             STMT_ENT_SET set = db->getAllRelationships(StmtNameRelationship::Uses);
             std::vector<int> vec;
             for (const STMT_ENT& se : set)
                 vec.push_back(se.first);
-            Result *result = new OneColResult<int>(synonymIdent, vec);
+            Result *result = new OneColResult<int>(first->str(), vec);
             return result;
         }
         case c(PQLToken::Tag::SYNONYM, PQLToken::Tag::IDENT):  // Uses(stmt, "x")/ -> int[]
         {
-            std::string synonymIdent = (dynamic_cast<const Synonym*>(first))->ident;
             STMT_SET stmtSet = db->getRelationship(StmtNameRelationship::Uses, second->str());
-            Result *result = new OneColResult<int>(synonymIdent, stmtSet);
+            Result *result = new OneColResult<int>(first->str(), stmtSet);
             return result;
         }
         default:
