169478b23bdd32a57a0cfd1f773616185f8fc472
Ng Jun Kang <ng_jun_kang@hotmail.com>
2023-02-16 20:36:48 +0800

    add validation for if and while

--- Team35/Code35/src/spa/src/sp/SyntaxValidator.cpp
+++ Team35/Code35/src/spa/src/sp/SyntaxValidator.cpp
@@ -29,10 +29,10 @@ bool SyntaxValidator::validateProgram() {
 bool SyntaxValidator::validateProc() {
     assert(peek(Token::Tag::Procedure));
     next();
-    if (!match(Token::Tag::Name)) {
+    if (!isName()) {
         return false;
     }
-
+    next();
     return validateStmtLst();
 }
 
@@ -51,12 +51,16 @@ bool SyntaxValidator::validateStmtLst() {
 }
 
 bool SyntaxValidator::validateStmt() {
-    if (peek(Token::Tag::Name)) {
+    if (peekOffset(Token::Tag::Assignment, 1) && isName()) {
         return validateAssign();
     } else if (peek(Token::Tag::Read)) {
         return validateRead();
     } else if (peek(Token::Tag::Print)) {
         return validatePrint();
+    } else if (peek(Token::Tag::If)) {
+        return validateIf();
+    } else if (peek(Token::Tag::While)) {
+        return validateWhile();
     } else {
         return false;
     }
@@ -95,6 +99,56 @@ bool SyntaxValidator::validatePrint() {
     return match(Token::Tag::SemiColon);
 }
 
+bool SyntaxValidator::validateIf() {
+    assert(peek(Token::Tag::If));
+    next();
+
+    if (!match(Token::Tag::LParen)) {
+        return false;
+    }
+
+    if (!validateCondExpr()) {
+        return false;
+    }
+
+    if (!match(Token::Tag::RParen)) {
+        return false;
+    }
+
+    if (!match(Token::Tag::Then)) {
+        return false;
+    }
+
+    if (!validateStmtLst()) {
+        return false;
+    }
+
+    if (!match(Token::Tag::Else)) {
+        return false;
+    }
+
+    return validateStmtLst();
+}
+
+bool SyntaxValidator::validateWhile() {
+    assert(peek(Token::Tag::While));
+    next();
+
+    if (!match(Token::Tag::LParen)) {
+        return false;
+    }
+
+    if (!validateCondExpr()) {
+        return false;
+    }
+
+    if (!match(Token::Tag::RParen)) {
+        return false;
+    }
+
+    return validateStmtLst();
+}
+
 bool SyntaxValidator::validateExpr() {
     if (!validateTerm()) {
         return false;
@@ -129,7 +183,7 @@ bool SyntaxValidator::validateTerm() {
 }
 
 bool SyntaxValidator::validateFactor() {
-    if (peek(Token::Tag::Name)) {
+    if (isName()) {
         return validateName();
     } else if (peek(Token::Tag::Integer)) {
         return validateInt();
@@ -144,31 +198,46 @@ bool SyntaxValidator::validateFactor() {
 }
 
 bool SyntaxValidator::validateName() {
-    if (peek(Token::Tag::Name) || peek(Token::Tag::Call) || peek(Token::Tag::Read) || peek(Token::Tag::Print)
-        || peek(Token::Tag::Procedure) || peek(Token::Tag::Else) || peek(Token::Tag::Then) || peek(Token::Tag::If)
-        || peek(Token::Tag::While)) {
+    if (isName()) {
         next();
         return true;
     }
     return false;
 }
 
+bool SyntaxValidator::isName() {
+    return peek(Token::Tag::Name) || peek(Token::Tag::Call) || peek(Token::Tag::Read) || peek(Token::Tag::Print)
+        || peek(Token::Tag::Procedure) || peek(Token::Tag::Else) || peek(Token::Tag::Then) || peek(Token::Tag::If)
+        || peek(Token::Tag::While);
+}
+
 bool SyntaxValidator::validateInt() {
     return match(Token::Tag::Integer);
 }
 
-int SyntaxValidator::peek(Token::Tag tag) {
-    return (token_lst_[cur_idx_]->getTag() == tag) ? 1 : 0;
+bool SyntaxValidator::peek(Token::Tag tag) {
+    return (token_lst_[cur_idx_]->getTag() == tag);
 }
 
-int SyntaxValidator::match(Token::Tag tag) {
+bool SyntaxValidator::match(Token::Tag tag) {
     if (token_lst_[cur_idx_]->getTag() == tag) {
         if (tag != Token::Tag::EndOfFile) {
             next();
         }
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
+}
+
+bool SyntaxValidator::peekOffset(Token::Tag tag, uint32_t offset) {
+    if (!isOffsetValid(offset)) {
+        return false;
+    }
+    return (token_lst_[cur_idx_ + offset]->getTag() == tag);
+}
+
+bool SyntaxValidator::isOffsetValid(uint32_t offset) {
+    return (cur_idx_ + offset < token_lst_.size());
 }
 
 void SyntaxValidator::next() {
@@ -178,4 +247,85 @@ void SyntaxValidator::next() {
 std::deque<std::unique_ptr<Token>> SyntaxValidator::getTokenLst() {
     std::reverse(token_lst_.begin(), token_lst_.end());
     return std::move(this->token_lst_);
+}
+
+bool SyntaxValidator::isCondExprSeparatedByLogicalOperator() {
+    uint32_t offset = 1;
+    int unmatched_paren = 1;
+    while (isOffsetValid(offset) && unmatched_paren > 0) {
+        if (peekOffset(Token::Tag::LParen, offset)) {
+            ++unmatched_paren;
+        } else if (peekOffset(Token::Tag::RParen, offset)) {
+            --unmatched_paren;
+        }
+        ++offset;
+    }
+    if (!isOffsetValid(offset)) {
+        return false;
+    }
+    bool is_logical_operator_exist =
+        peekOffset(Token::Tag::LogicalAnd, offset) || peekOffset(Token::Tag::LogicalOr, offset);
+    return is_logical_operator_exist;
+}
+
+bool SyntaxValidator::validateCondExpr() {
+    if (peek(Token::Tag::LogicalNot)) {
+        next();
+        if (!match(Token::Tag::LParen)) {
+            return false;
+        }
+
+        if (!validateCondExpr()) {
+            return false;
+        }
+
+        return match(Token::Tag::RParen);
+    } else if (peek(Token::Tag::LParen)) {
+        if (!isCondExprSeparatedByLogicalOperator()) {
+            return validateRelExpr();
+        }
+
+        // && || exist
+        if (!match(Token::Tag::LParen)) {
+            return false;
+        }
+
+        if (!validateCondExpr()) {
+            return false;
+        }
+
+        if (!match(Token::Tag::RParen)) {
+            return false;
+        }
+
+        if (!match(Token::Tag::LogicalAnd) && !match(Token::Tag::LogicalOr)) {
+            return false;
+        }
+
+        if (!match(Token::Tag::LParen)) {
+            return false;
+        }
+
+        if (!validateCondExpr()) {
+            return false;
+        }
+
+        return match(Token::Tag::RParen);
+    } else {
+        return validateRelExpr();
+    }
+}
+
+bool SyntaxValidator::validateRelExpr() {
+    if (!validateExpr()) {
+        return false;
+    }
+
+    if (!match(Token::Tag::Equivalence) && !match(Token::Tag::NotEqual) && !match(Token::Tag::LessThan)
+        && !match(Token::Tag::LessThanEqualTo) && !match(Token::Tag::GreaterThan)
+        && !match(Token::Tag::GreaterThanEqualTo)) {
+        return false;
+    }
+
+    return validateExpr();
 }
