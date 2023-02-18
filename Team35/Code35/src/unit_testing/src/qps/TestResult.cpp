64f2c8c280ca16cb862445f039bde86223e47eee
nguyiyang <nguyiyang47@gmail.com>
2023-02-18 12:56:07 +0800

    fix linter issues

--- Team35/Code35/src/unit_testing/src/qps/TestResult.cpp
+++ Team35/Code35/src/unit_testing/src/qps/TestResult.cpp
@@ -2,6 +2,9 @@
 #include "../TestHelper.h"
 #include "qps/result/TableResult.h"
 #include "qps/result/BoolResult.h"
+#include "qps/query_parser/QueryParser.h"
+#include "commons/lexer/ILexer.h"
+#include "commons/lexer/LexerFactory.h"
 
 TEST_CASE("Test join: 1 common column") {
     std::list<std::string> header1 = {"s", "v"};
@@ -26,45 +29,77 @@ TEST_CASE("Test join: 1 common column") {
     requireTrue(*expectedResult == *(Result::join(tableResult1, tableResult2)));
 }
 
-//TEST_CASE("Test join: true and table") {
-//    BoolResult* boolResult = new BoolResult(true);
-//
-//    std::list<std::string> header2 = {"a", "v"};
-//    std::vector<std::list<std::string>> rows2;
-//    rows2.push_back({"1", "x"});
-//    rows2.push_back({"1", "z"});
-//    TableResult* tableResult2 = new TableResult(header2, rows2);
-//
-//    std::list<std::string> resultHeader = {"a", "v"};
-//    std::vector<std::list<std::string>> resultRows;
-//    resultRows.push_back({"1", "x"});
-//    resultRows.push_back({"1", "z"});
-//    TableResult* expectedResult = new TableResult(resultHeader, resultRows);
-//
-//    Result* result;
-//
-//    requireTrue(*expectedResult == *(result->join(tableResult2, boolResult)));
-//}
+TEST_CASE("Test join: true and true") {
+    BoolResult* boolResult1 = new BoolResult(true);
 
-//TEST_CASE("Test join: false and table") {
-//    BoolResult* boolResult;
-//    boolResult->b = false;
-//
-//    std::list<std::string> header2 = {"a", "v"};
-//    std::vector<std::list<std::string>> rows2;
-//    rows2.push_back({"1", "x"});
-//    rows2.push_back({"1", "z"});
-//    TableResult* tableResult2;
-//    tableResult2->idents = header2;
-//    tableResult2->rows = rows2;
-//
-//    BoolResult* expectedResult = new BoolResult(false);
-//
-//    Result* results = expectedResult->join(boolResult, tableResult2);
-//
-//    requireTrue(*expectedResult == *results);
-//}
+    BoolResult* boolResult2 = new BoolResult(true);
 
+    BoolResult* expectedResult = new BoolResult(true);
+
+    requireTrue(*expectedResult == *(Result::join(boolResult1, boolResult2)));
+}
+
+TEST_CASE("Test join: true and false") {
+    BoolResult* boolResult1 = new BoolResult(true);
+
+    BoolResult* boolResult2 = new BoolResult(false);
+
+    BoolResult* expectedResult = new BoolResult(false);
+
+    requireTrue(*expectedResult == *(Result::join(boolResult1, boolResult2)));
+}
+
+TEST_CASE("Test join: false and true") {
+    BoolResult* boolResult1 = new BoolResult(false);
+
+    BoolResult* boolResult2 = new BoolResult(true);
+
+    BoolResult* expectedResult = new BoolResult(false);
+
+    requireTrue(*expectedResult == *(Result::join(boolResult1, boolResult2)));
+}
+
+TEST_CASE("Test join: false and false") {
+    BoolResult* boolResult1 = new BoolResult(false);
+
+    BoolResult* boolResult2 = new BoolResult(false);
+
+    BoolResult* expectedResult = new BoolResult(false);
+
+    requireTrue(*expectedResult == *(Result::join(boolResult1, boolResult2)));
+}
+
+TEST_CASE("Test join: true and table") {
+    BoolResult* boolResult = new BoolResult(true);
+
+    std::list<std::string> header2 = {"a", "v"};
+    std::vector<std::list<std::string>> rows2;
+    rows2.push_back({"1", "x"});
+    rows2.push_back({"1", "z"});
+    TableResult* tableResult2 = new TableResult(header2, rows2);
+
+    std::list<std::string> resultHeader = {"a", "v"};
+    std::vector<std::list<std::string>> resultRows;
+    resultRows.push_back({"1", "x"});
+    resultRows.push_back({"1", "z"});
+    TableResult* expectedResult = new TableResult(resultHeader, resultRows);
+
+    requireTrue(*expectedResult == *(Result::join(boolResult, tableResult2)));
+}
+
+TEST_CASE("Test join: false and table") {
+    BoolResult* boolResult = new BoolResult(false);
+
+    std::list<std::string> header2 = {"a", "v"};
+    std::vector<std::list<std::string>> rows2;
+    rows2.push_back({"1", "x"});
+    rows2.push_back({"1", "z"});
+    TableResult* tableResult2 = new TableResult(header2, rows2);
+
+    BoolResult* expectedResult = new BoolResult(false);
+
+    requireTrue(*expectedResult == *(Result::join(boolResult, tableResult2)));
+}
 
 TEST_CASE("Test join: 2 common columns") {
     std::list<std::string> header1 = {"s", "v", "c"};
@@ -153,4 +188,41 @@ TEST_CASE("Test join: no common columns, 3x2") {
     TableResult* expectedResult = new TableResult(resultHeader, resultRows);
 
     requireTrue(*expectedResult == *(Result::join(tableResult1, tableResult2)));
+}
+
+TEST_CASE("Test output") {
+    std::list<std::string> result;
+    std::list<std::string> header1 = {"s", "a", "y"};
+    std::vector<std::list<std::string>> rows1;
+    rows1.push_back({"1", "1", "4"});
+    rows1.push_back({"2", "1", "4"});
+    TableResult* tableResult1 = new TableResult(header1, rows1);
+
+    std::list<std::string> header2 = {"a", "b"};
+    std::vector<std::list<std::string>> rows2;
+    rows2.push_back({"1", "x"});
+    rows2.push_back({"2", "x"});
+    TableResult* tableResult2 = new TableResult(header2, rows2);
+
+    Result* resultTable = Result::join(tableResult1, tableResult2);
+    resultTable->output(result, "a");
+    std::list<std::string> expectedResult;
+    expectedResult.push_back("1");
+    requireEqual(expectedResult, result);
+}
+
+TEST_CASE("Test output selected does not match") {
+    std::list<std::string> result;
+    BoolResult* boolResult = new BoolResult(false);
+
+    std::list<std::string> header2 = {"a", "b"};
+    std::vector<std::list<std::string>> rows2;
+    rows2.push_back({"1", "x"});
+    rows2.push_back({"2", "x"});
+    TableResult* tableResult2 = new TableResult(header2, rows2);
+
+    Result* resultTable = Result::join(boolResult, tableResult2);
+    resultTable->output(result, "x");
+
+    requireTrue(result.size() == 0);
 }
