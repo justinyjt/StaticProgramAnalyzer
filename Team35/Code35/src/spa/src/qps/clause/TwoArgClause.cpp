75144a63ef7f955b55d4906121f18f128255f2f9
Ng Jun Kang <ng_jun_kang@hotmail.com>
2023-02-19 19:26:48 +0800

    fix all build warnings

--- Team35/Code35/src/spa/src/qps/clause/TwoArgClause.cpp
+++ Team35/Code35/src/spa/src/qps/clause/TwoArgClause.cpp
@@ -1,24 +1,19 @@
 #include "TwoArgClause.h"
 
+#include <cassert>
+
 TwoArgClause::TwoArgClause(std::unique_ptr<PQLToken> first,
-                           std::unique_ptr<PQLToken> second) :first(std::move(first)), second(std::move(second)) {}
+                           std::unique_ptr<PQLToken> second) : first(std::move(first)), second(std::move(second)) {}
 
 StmtType TwoArgClause::getStmtType(Synonym::DesignEntity type) {
     switch (type) {
-        case Synonym::DesignEntity::READ:
-            return StmtType::Read;
-        case Synonym::DesignEntity::PRINT:
-            return StmtType::Print;
-        case Synonym::DesignEntity::WHILE:
-            return StmtType::While;
-        case Synonym::DesignEntity::IF:
-            return StmtType::If;
-        case Synonym::DesignEntity::ASSIGN:
-            return StmtType::Assign;
-        case Synonym::DesignEntity::STMT:
-            return StmtType::None;
-        default:
-            assert(false);
+        case Synonym::DesignEntity::READ:return StmtType::Read;
+        case Synonym::DesignEntity::PRINT:return StmtType::Print;
+        case Synonym::DesignEntity::WHILE:return StmtType::While;
+        case Synonym::DesignEntity::IF:return StmtType::If;
+        case Synonym::DesignEntity::ASSIGN:return StmtType::Assign;
+        case Synonym::DesignEntity::STMT:return StmtType::None;
+        default:assert(false);
     }
 }
 
--- Team35/Code35/src/spa/src/qps/clause/Uses.h
+++ Team35/Code35/src/spa/src/qps/clause/Uses.h
@@ -1,13 +1,14 @@
 #pragma once
 #include <memory>
 #include <utility>
+
 #include "TwoArgClause.h"
 
 class UsesS : public TwoArgClause {
  public:
     /* <SYNONYM | STMT_NUM>, <SYNONYM | _ | IDENT> */
     UsesS(std::unique_ptr<PQLToken> first, std::unique_ptr<PQLToken> second);
-    std::unique_ptr<Result> evaluate(PKBReader*) override;
+    std::unique_ptr<Result> evaluate(PKBReader *) override;
     void validateArgs() override;
-    bool operator==(const Clause& rhs) const override;
+    bool operator==(const Clause &rhs) const override;
 };
