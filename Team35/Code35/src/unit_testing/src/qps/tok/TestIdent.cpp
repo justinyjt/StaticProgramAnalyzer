e027e56ff36bf4827354feb3aa525698b3128af8
Ng Jun Kang <ng_jun_kang@hotmail.com>
2023-02-19 20:21:50 +0800

    fix possible mem leaks

--- Team35/Code35/src/unit_testing/src/qps/tok/TestIdent.cpp
+++ Team35/Code35/src/unit_testing/src/qps/tok/TestIdent.cpp
@@ -4,8 +4,8 @@
 #include "qps/pql/PQLToken.h"
 
 TEST_CASE("Ident") {
-    Ident* hi = new Ident("hi");
-    Ident* bye = new Ident("bye");
+    Ident *hi = new Ident("hi");
+    Ident *bye = new Ident("bye");
     std::unique_ptr<PQLToken> hi_ = std::make_unique<Ident>("hi");
 
     requireTrue(*hi == *hi_);
@@ -14,4 +14,6 @@ TEST_CASE("Ident") {
     requireEqual(hi->tag, PQLToken::Tag::IDENT);
     requireEqual(hi->s, std::string("hi"));
     requireEqual(hi->str(), std::string("hi"));
+    delete hi;
+    delete bye;
 }
