5333c5815eadf8488937668a93846861ce521433
pangyuanker <e0660595@u.nus.edu>
2023-03-03 14:56:22 +0800

    Add storage functionality for calls in PKB

--- Team35/Code35/src/spa/src/commons/types.h
+++ Team35/Code35/src/spa/src/commons/types.h
@@ -11,14 +11,14 @@ typedef std::string ENT_NAME;
 typedef int STMT_NUM;
 typedef std::uint32_t LineNumber;
 typedef std::pair<STMT_NUM, ENT_NAME> STMT_ENT;
-typedef std::pair<ENT_NAME , ENT_NAME> ENT_ENT;
+typedef std::pair<ENT_NAME, ENT_NAME> ENT_ENT;
 typedef std::pair<STMT_NUM, STMT_NUM> STMT_STMT;
 
 typedef std::unordered_set<STMT_NUM> STMT_SET;
 typedef std::unordered_set<ENT_NAME> ENT_SET;
 typedef std::unordered_set<STMT_ENT, hash_pair> STMT_ENT_SET;
 typedef std::unordered_set<ENT_ENT, hash_pair> ENT_ENT_SET;
-typedef std::unordered_set<STMT_STMT , hash_pair> STMT_STMT_SET;
+typedef std::unordered_set<STMT_STMT, hash_pair> STMT_STMT_SET;
 
 
 enum class StmtType {
@@ -30,7 +30,7 @@ enum class StmtNameRelationship {
 };
 
 enum class NameNameRelationship {
-    Uses, Modifies, None
+    Uses, Modifies, Calls, CallsT, None
 };
 
 enum class StmtStmtRelationship {
--- Team35/Code35/src/spa/src/pkb/PKB.h
+++ Team35/Code35/src/spa/src/pkb/PKB.h
@@ -17,42 +17,69 @@ class TNode;
 class PKB {
  public:
     const EntityTable<ENT_NAME> &getEntityTable(Entity entityType) const;
+
     const EntityTable<STMT_NUM> &getStatementTable(StmtType stmtType) const;
+
     const RelationshipTable<STMT_NUM, ENT_NAME> &getStmtNameRelationshipTable(StmtNameRelationship tableType) const;
+
     const RelationshipTable<ENT_NAME, ENT_NAME> &getNameNameRelationshipTable(NameNameRelationship tableType) const;
+
     const RelationshipTable<STMT_NUM, STMT_NUM> &getStmtStmtRelationshipTable(StmtStmtRelationship tableType) const;
+
     const PatternTable &getPatternTable() const;
 
     RelationshipTable<STMT_NUM, ENT_NAME> &getStmtNameRelationshipTable(StmtNameRelationship tableType);
+
     RelationshipTable<ENT_NAME, ENT_NAME> &getNameNameRelationshipTable(NameNameRelationship tableType);
+
     RelationshipTable<STMT_NUM, STMT_NUM> &getStmtStmtRelationshipTable(StmtStmtRelationship tableType);
+
     EntityTable<ENT_NAME> &getEntityTable(Entity entityType);
+
     EntityTable<STMT_NUM> &getStatementTable(StmtType stmtType);
+
     PatternTable &getPatternTable();
 
     bool addEntityToTable(Entity entityType, ENT_NAME entity);
+
     bool addStatementToTable(StmtType stmtType, STMT_NUM stmt);
+
     bool addRelationshipToTable(StmtNameRelationship tableType, STMT_ENT stmtEnt);
+
     bool addRelationshipToTable(NameNameRelationship tableType, ENT_ENT entEnt);
+
     bool addRelationshipToTable(StmtStmtRelationship tableType, STMT_STMT stmtStmt);
+
     bool addPattern(STMT_NUM stmtNum, std::string pattern);
 
     ENT_SET getEntByStmtKey(StmtNameRelationship tableType, STMT_NUM stmt) const;
+
     STMT_SET getStmtByEntVal(StmtNameRelationship tableType, ENT_NAME name) const;
+
     STMT_ENT_SET getStmtEntSet(StmtNameRelationship tableType) const;
+
     STMT_SET getStmtByRs(StmtNameRelationship tableType) const;
+
     bool isStmtEntPairExists(StmtNameRelationship tableType, STMT_NUM stmt, ENT_NAME name) const;
 
     ENT_SET getEntByEntKey(NameNameRelationship tableType, ENT_NAME name) const;
+
     ENT_SET getEntByEntVal(NameNameRelationship tableType, ENT_NAME name) const;
+
     ENT_ENT_SET getEntEntSet(NameNameRelationship tableType) const;
+
     bool isEntEntPairExists(NameNameRelationship tableType, ENT_NAME key, ENT_NAME val) const;
 
     STMT_SET getStmtByStmtKey(StmtStmtRelationship tableType, STMT_NUM stmt) const;
+
     STMT_SET getStmtByStmtVal(StmtStmtRelationship tableType, STMT_NUM stmt) const;
+
     STMT_STMT_SET getStmtStmtSet(StmtStmtRelationship tableType) const;
+
     STMT_SET getKeyStmtByRs(StmtStmtRelationship tableType) const;
+
     STMT_SET getValStmtByRs(StmtStmtRelationship tableType) const;
+
     bool isStmtStmtPairExists(StmtStmtRelationship tableType, STMT_NUM key, STMT_NUM val) const;
 
 
@@ -65,13 +92,16 @@ class PKB {
     EntityTable<STMT_NUM> whileStatementTable;
     EntityTable<STMT_NUM> printStatementTable;
     EntityTable<STMT_NUM> readStatementTable;
+    EntityTable<STMT_NUM> callStatementTable;
     EntityTable<STMT_NUM> statementTable;
 
     // Relationship related tables
     RelationshipTable<STMT_NUM, ENT_NAME> modifiesStmtNameTable;
     RelationshipTable<STMT_NUM, ENT_NAME> usesStmtNameTable;
-    RelationshipTable<ENT_NAME , ENT_NAME> modifiesNameNameTable;
-    RelationshipTable<ENT_NAME , ENT_NAME> usesNameNameTable;
+    RelationshipTable<ENT_NAME, ENT_NAME> modifiesNameNameTable;
+    RelationshipTable<ENT_NAME, ENT_NAME> usesNameNameTable;
+    RelationshipTable<ENT_NAME, ENT_NAME> callsNameNameTable;
+    RelationshipTable<ENT_NAME, ENT_NAME> callsTransitiveNameNameTable;
 
     RelationshipTable<STMT_NUM, STMT_NUM> followsTable;
     RelationshipTable<STMT_NUM, STMT_NUM> followsStarTable;
