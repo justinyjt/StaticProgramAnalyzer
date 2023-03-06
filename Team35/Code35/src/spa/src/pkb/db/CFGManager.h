52032e768a3fab7614191cefa1faca782581949f
pangyuanker <e0660595@u.nus.edu>
2023-03-06 23:08:41 +0800

    Implement CFG Manager in PKB

---
+++ Team35/Code35/src/spa/src/pkb/db/CFGManager.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "commons/graph/CFGraph.h"
+
+namespace CFG {
+
+    class CFGManager {
+     private:
+        std::vector<CFGraph> graphs_;
+
+        int getIndex(STMT_NUM num);
+
+     public:
+        CFGManager();
+
+        void setGraphs(std::vector<CFGraph> &&cfg);
+
+        const CFGraph &getCFG(STMT_NUM num);
+
+        ENT_NAME getProcName(STMT_NUM num);
+
+    };
+}
