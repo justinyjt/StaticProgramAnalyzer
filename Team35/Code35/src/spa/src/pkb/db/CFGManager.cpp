652b69a5f8a6f3ff6aa4b0913bc02eef20105289
pangyuanker <e0660595@u.nus.edu>
2023-03-12 11:41:05 +0800

    Implement Next and Next*

--- Team35/Code35/src/spa/src/pkb/db/CFGManager.cpp
+++ Team35/Code35/src/spa/src/pkb/db/CFGManager.cpp
@@ -41,4 +41,54 @@ ENT_NAME CFGManager::getProcName(STMT_NUM num) {
 bool CFGManager::isValidStmtNum(STMT_NUM num) {
     return this->getIndex(num) != -1;
 }
+
+bool CFGManager::isNext(STMT_NUM first, STMT_NUM second, bool isTransitive) {
+    if (!isValidStmtNum(first) || !isValidStmtNum(second)) {
+        return false;
+    }
+    const CFGraph &curr = getCFG(first);
+    if (isTransitive) {
+        return curr.isReachable(first, second);
+    }
+    STMT_SET successors = curr.getSuccessors(first, isTransitive);
+    for (auto successor : successors) {
+        if (second == successor) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool CFGManager::isNextExists() {
+    for (auto &curr : this->graphs_) {
+        if (curr.getMaxStmtNum() - curr.getMinStmtNum() > 1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+STMT_SET CFGManager::getConnectedStmts(STMT_NUM num, bool isFirstArg, bool isTransitive) {
+    const CFGraph &curr = getCFG(num);
+    if (isFirstArg) {
+        return curr.getSuccessors(num, isTransitive);
+    }
+    return curr.getPredecessors(num, isTransitive);
+}
+
+bool CFGManager::isNextExistByKey(STMT_NUM num, bool isTransitive) {
+    return !getConnectedStmts(num, true, isTransitive).empty();
+}
+
+bool CFGManager::isNextExistByVal(STMT_NUM num, bool isTransitive) {
+    return !getConnectedStmts(num, false, isTransitive).empty();
+}
+
+STMT_STMT_SET CFGManager::getValidNextPairs(bool isTransitive) {
+    STMT_STMT_SET result;
+    for (auto &curr : this->graphs_) {
+        STMT_STMT_SET temp = curr.getPairwiseControlFlow(isTransitive);
+        result.insert(temp.begin(), temp.end());
+    }
+}
 }  // namespace CFG
--- Team35/Code35/src/spa/src/pkb/db/CFGManager.h
+++ Team35/Code35/src/spa/src/pkb/db/CFGManager.h
@@ -22,5 +22,17 @@ class CFGManager {
     ENT_NAME getProcName(STMT_NUM num);
 
     bool isValidStmtNum(STMT_NUM num);
+
+    bool isNext(STMT_NUM first, STMT_NUM second, bool isTransitive);
+
+    bool isNextExists();
+
+    STMT_SET getConnectedStmts(STMT_NUM num, bool isFirstArg, bool isTransitive);
+
+    bool isNextExistByKey(STMT_NUM num, bool isTransitive);
+
+    bool isNextExistByVal(STMT_NUM num, bool isTransitive);
+
+    STMT_STMT_SET getValidNextPairs(bool isTransitive);
 };
 }  // namespace CFG
