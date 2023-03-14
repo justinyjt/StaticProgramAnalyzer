cf38bcb51f31983c64b20bbc1d99c7e312ea3270
Dave Low <davelowqx@gmail.com>
2023-03-14 16:39:58 +0800

    first commit if/while eval

--- Team35/Code35/src/spa/src/qps/clause/one_arg_clause/IfPattern.cpp
+++ Team35/Code35/src/spa/src/qps/clause/one_arg_clause/IfPattern.cpp
@@ -8,7 +8,34 @@ IfPattern::IfPattern(std::unique_ptr<PQLToken> first,
 }
 
 std::unique_ptr<Result> IfPattern::evaluate(PKBReader *db) {
-    throw std::runtime_error("IfPattern.cpp");
+    STMT_SET ifStatements = db->getStatements(StmtType::If);
+    switch (first->tag) {
+        case PQLToken::Tag::WILDCARD: {  // ifs(_, _, _) -> int[]
+            return std::make_unique<TableResult>(ifStatements);
+        }
+        case PQLToken::Tag::IDENT: {  // ifs("x", _, _) -> int[]
+            STMT_SET s = db->getRelationship(StmtNameRelationship::Uses, 
+                                dynamic_cast<Ident&>(*first).s);
+            STMT_SET result;
+            for (int stmt : ifStatements) {
+                if (s.find(stmt) != s.end()) {
+                    result.insert(stmt);
+                }
+            }
+            return std::make_unique<TableResult>(result);
+        }
+        case PQLToken::Tag::SYNONYM: {  // ifs(x, _, _) -> <int, str>[]
+            STMT_ENT_SET se = db->getAllRelationships(StmtNameRelationship::Uses);
+            STMT_ENT_SET result;
+            for (auto& p : se) {
+                if (ifStatements.find(p.first) != ifStatements.end()) {
+                    result.insert(p);
+                }
+            }
+            return std::make_unique<TableResult>(result);
+        }
+        default: throw std::runtime_error("IfPattern");
+    }
 }
 
 void IfPattern::validateArgs() {
--- Team35/Code35/src/spa/src/qps/clause/one_arg_clause/WhilePattern.cpp
+++ Team35/Code35/src/spa/src/qps/clause/one_arg_clause/WhilePattern.cpp
@@ -8,7 +8,34 @@ WhilePattern::WhilePattern(std::unique_ptr<PQLToken> first,
 }
 
 std::unique_ptr<Result> WhilePattern::evaluate(PKBReader *db) {
-    throw std::runtime_error("WhilePattern.cpp");
+    STMT_SET whileStatements = db->getStatements(StmtType::While);
+    switch (first->tag) {
+        case PQLToken::Tag::WILDCARD: {  // w(_, _) -> int[]
+            return std::make_unique<TableResult>(whileStatements);
+        }
+        case PQLToken::Tag::IDENT: {  // w("x", _) -> int[]
+            STMT_SET s = db->getRelationship(StmtNameRelationship::Uses, 
+                                dynamic_cast<Ident&>(*first).s);
+            STMT_SET result;
+            for (int stmt : whileStatements) {
+                if (s.find(stmt) != s.end()) {
+                    result.insert(stmt);
+                }
+            }
+            return std::make_unique<TableResult>(result);
+        }
+        case PQLToken::Tag::SYNONYM: {  // w(x, _) -> <int, str>[]
+            STMT_ENT_SET se = db->getAllRelationships(StmtNameRelationship::Uses);
+            STMT_ENT_SET result;
+            for (auto& p : se) {
+                if (whileStatements.find(p.first) != whileStatements.end()) {
+                    result.insert(p);
+                }
+            }
+            return std::make_unique<TableResult>(result);
+        }
+        default: throw std::runtime_error("WhilePattern");
+    }
 }
 
 void WhilePattern::validateArgs() {
