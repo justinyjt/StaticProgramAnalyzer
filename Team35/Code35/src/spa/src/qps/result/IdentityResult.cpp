e36b980618a4af202bde503e5cb3bd9be71193c1
nguyiyang <nguyiyang47@gmail.com>
2023-03-28 15:30:50 +0800

    Implement join method for IdentityResult

--- Team35/Code35/src/spa/src/qps/result/IdentityResult.cpp
+++ Team35/Code35/src/spa/src/qps/result/IdentityResult.cpp
@@ -7,6 +7,7 @@ IdentityResult::IdentityResult() : Result(Tag::IDENTITY) {
 }
 
 std::unique_ptr<Result> IdentityResult::join(Result &rhs) {
+    throw std::runtime_error("Identity join should not be called");
 }
 
 void IdentityResult::output(std::list<std::string>& list) {
