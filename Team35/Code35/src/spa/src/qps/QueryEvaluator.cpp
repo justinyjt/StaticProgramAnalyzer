9f21316e96919ed841b226801a7efe9d636462eb
Ng Jun Kang <45599592+ngjunkang@users.noreply.github.com>
2023-04-02 15:53:39 +0800

    Refactor query evaluator into methods (#165)
    * refactor query evaluator
    
    * clean arguments: unneccessary move

--- Team35/Code35/src/spa/src/qps/QueryEvaluator.cpp
+++ Team35/Code35/src/spa/src/qps/QueryEvaluator.cpp
@@ -8,9 +8,8 @@
 
 QueryEvaluator::QueryEvaluator(PKBReader *db) : db(db) {}
 
-std::unique_ptr<Result> QueryEvaluator::evaluate(std::unique_ptr<SelectClause> selectClause,
-                                                 std::vector<std::unique_ptr<OptimisableClause>> clauses) const {
-    bool isBooleanSelect = selectClause->type_ == SelectClause::SelectType::Boolean;
+std::unique_ptr<Result> QueryEvaluator::evaluate(const std::unique_ptr<SelectClause> &selectClause,
+                                                 const std::vector<std::unique_ptr<OptimisableClause>> &clauses) const {
     const HEADER_SET &selectHeaders = selectClause->getHeaders();
     std::unique_ptr<Result> selectRes = selectClause->evaluate(db);
 
@@ -25,38 +24,72 @@ std::unique_ptr<Result> QueryEvaluator::evaluate(std::unique_ptr<SelectClause> s
     // clause evaluation
     // individual clause evaluation based on order determined by ClauseOptimiser
     ClauseIndexList clauseEvaluationOrder = clauseOptimiser.getClauseEvaluationOrder();
-    bool isAnyClauseEmpty = false;
-    bool hasHeaders = false;
-    // TODO(ngjunkang): evaluate clauses in parallel
-    for (ClauseIndex clauseIndex : clauseEvaluationOrder) {
-        results[clauseIndex] = clauses[clauseIndex]->evaluate(db);
-        hasHeaders = hasHeaders || !clauses[clauseIndex]->getHeaders().empty();
-        if (results[clauseIndex]->isNull()) {
-            isAnyClauseEmpty = true;
-            break;
-        }
-    }
+    bool isAnyResultNull = !evaluateIndividualClauses(clauses, clauseEvaluationOrder, results);
     db->clearCache();
-    if (isAnyClauseEmpty) {
+    if (isAnyResultNull) {
         BoolResult falseResult(false);
         return selectRes->join(falseResult);
-    }
-
-    if (!hasHeaders) {
-        BoolResult trueResult(true);
-        return selectRes->join(trueResult);
     }
 
     // Clause group joining - join evaluated results within each group
     // based on order determined by ClauseOptimiser
     ClauseGroupList clauseGroups = clauseOptimiser.getClauseGroups();
     if (clauseGroups.empty()) {
+        IdentityResult identRes;
+        return selectRes->join(identRes);
+    }
+
+    std::vector<std::unique_ptr<Result>> groupedIntermediateResults(clauseGroups.size());
+    bool isAnyIntermediateResultNull =
+            !joinInClauseGroups(std::move(results), clauseGroups, groupedIntermediateResults);
+    if (isAnyIntermediateResultNull) {
         BoolResult falseResult(false);
         return selectRes->join(falseResult);
     }
 
-    std::vector<std::unique_ptr<Result>> groupedIntermediateResults(clauseGroups.size());
-    bool isAnyClauseGroupEmpty = false;
+    // skip joining of groups if boolean select
+    bool isBooleanSelect = selectClause->type_ == SelectClause::SelectType::Boolean;
+    if (isBooleanSelect) {
+        BoolResult trueResult(true);
+        return selectRes->join(trueResult);
+    }
+
+    // clean up the grouped results, so that only required columns are kept
+    std::vector<std::unique_ptr<Result>>
+            cleanedIntermediateResults = cleanIntermediateResults(groupedIntermediateResults,
+                                                                  clauseGroups,
+                                                                  selectHeaders);
+
+    // if no groups left
+    if (cleanedIntermediateResults.empty()) {
+        IdentityResult identResult;
+        return selectRes->join(identResult);
+    }
+
+    // clause grouping - join between groups
+    std::unique_ptr<Result> finalClauseResult = joinAllResults(std::move(cleanedIntermediateResults));
+
+    // finally, join with the select clause
+    std::unique_ptr<Result> finalRes = selectRes->join(*finalClauseResult);
+    return std::move(finalRes);
+}
+
+bool QueryEvaluator::evaluateIndividualClauses(const std::vector<std::unique_ptr<OptimisableClause>> &clauses,
+                                               const ClauseIndexList &clauseEvaluationOrder,
+                                               std::vector<std::unique_ptr<Result>> &results) const {
+    // TODO(ngjunkang): evaluate clauses in parallel
+    for (ClauseIndex clauseIndex : clauseEvaluationOrder) {
+        results[clauseIndex] = clauses[clauseIndex]->evaluate(db);
+        if (results[clauseIndex]->isNull()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool QueryEvaluator::joinInClauseGroups(std::vector<std::unique_ptr<Result>> results,
+                                        const ClauseGroupList &clauseGroups,
+                                        std::vector<std::unique_ptr<Result>> &intermediateResults) {
     for (int i = 0; i < clauseGroups.size(); ++i) {
         const auto &joinOrder = clauseGroups[i].getClauseJoinOrder();
         // join all clauses in the group
@@ -66,29 +99,18 @@ std::unique_ptr<Result> QueryEvaluator::evaluate(std::unique_ptr<SelectClause> s
             std::unique_ptr<Result> merged = curr->join(*next);
             curr = std::move(merged);
             if (curr->isNull()) {
-                isAnyClauseGroupEmpty = true;
-                break;
+                return false;
             }
         }
-
-        if (isAnyClauseGroupEmpty) {
-            break;
-        }
-        groupedIntermediateResults[i] = std::move(curr);
+        intermediateResults[i] = std::move(curr);
     }
+    return true;
+}
 
-    if (isAnyClauseGroupEmpty) {
-        BoolResult falseResult(false);
-        return selectRes->join(falseResult);
-    }
-
-    // skip joining of groups if boolean select
-    if (isBooleanSelect) {
-        BoolResult trueResult(true);
-        return selectRes->join(trueResult);
-    }
-
-    // clean up the grouped results, so that only required columns are kept
+std::vector<std::unique_ptr<Result>> QueryEvaluator::cleanIntermediateResults(
+        const std::vector<std::unique_ptr<Result>> &intermediateResults,
+        const ClauseGroupList &clauseGroups,
+        const HEADER_SET &selectHeaders) {
     std::vector<std::unique_ptr<Result>> cleanedIntermediateResults;
     for (int i = 0; i < clauseGroups.size(); ++i) {
         const auto &groupHeaders = clauseGroups[i].getGroupHeaders();
@@ -96,28 +118,21 @@ std::unique_ptr<Result> QueryEvaluator::evaluate(std::unique_ptr<SelectClause> s
         if (!hasIntersection) {
             continue;
         }
-        auto &rawIntermediateResult = dynamic_cast<TableResult &>(*groupedIntermediateResults[i]);
+        auto &rawIntermediateResult = dynamic_cast<TableResult &>(*intermediateResults[i]);
         cleanedIntermediateResults.emplace_back(rawIntermediateResult.projectColumns(selectHeaders));
     }
+    return std::move(cleanedIntermediateResults);
+}
 
-    // if no groups left
-    if (cleanedIntermediateResults.empty()) {
-        IdentityResult identResult;
-        return selectRes->join(identResult);
-    }
-
-    // clause grouping - join between groups
-    std::unique_ptr<Result> finalClauseResult = std::move(cleanedIntermediateResults.front());
-    for (int i = 1; i < cleanedIntermediateResults.size(); ++i) {
-        std::unique_ptr<Result> next = std::move(cleanedIntermediateResults[i]);
-        std::unique_ptr<Result> merged = finalClauseResult->join(*next);
-        finalClauseResult = std::move(merged);
-        if (finalClauseResult->isNull()) {
+std::unique_ptr<Result> QueryEvaluator::joinAllResults(std::vector<std::unique_ptr<Result>> results) {
+    std::unique_ptr<Result> finalResult = std::move(results.front());
+    for (int i = 1; i < results.size(); ++i) {
+        std::unique_ptr<Result> next = std::move(results[i]);
+        std::unique_ptr<Result> merged = finalResult->join(*next);
+        finalResult = std::move(merged);
+        if (finalResult->isNull()) {
             break;
         }
     }
-
-    // finally, join with the select clause
-    std::unique_ptr<Result> finalRes = selectRes->join(*finalClauseResult);
-    return std::move(finalRes);
+    return std::move(finalResult);
 }
--- Team35/Code35/src/spa/src/qps/QueryEvaluator.h
+++ Team35/Code35/src/spa/src/qps/QueryEvaluator.h
@@ -7,13 +7,30 @@
 
 #include "qps/clause/OptimisableClause.h"
 #include "qps/clause/select_clause/SelectClause.h"
+#include "qps/clause_optimiser/ClauseOptimiser.h"
 
 class QueryEvaluator {
  public:
     explicit QueryEvaluator(PKBReader *);
-    std::unique_ptr<Result> evaluate(std::unique_ptr<SelectClause> selectClause,
-                                     std::vector<std::unique_ptr<OptimisableClause>> clauses) const;
+    [[nodiscard]] std::unique_ptr<Result> evaluate(
+            const std::unique_ptr<SelectClause> &selectClause,
+            const std::vector<std::unique_ptr<OptimisableClause>> &clauses) const;
 
  private:
     PKBReader *db;
+
+    bool evaluateIndividualClauses(const std::vector<std::unique_ptr<OptimisableClause>> &clauses,
+                                   const ClauseIndexList &clauseEvaluationOrder,
+                                   std::vector<std::unique_ptr<Result>> &results) const;
+
+    static bool joinInClauseGroups(std::vector<std::unique_ptr<Result>> results,
+                                   const ClauseGroupList &clauseGroups,
+                                   std::vector<std::unique_ptr<Result>> &intermediateResults);
+
+    static std::vector<std::unique_ptr<Result>> cleanIntermediateResults(
+            const std::vector<std::unique_ptr<Result>> &intermediateResults,
+            const ClauseGroupList &clauseGroups,
+            const HEADER_SET &selectHeaders);
+
+    static std::unique_ptr<Result> joinAllResults(std::vector<std::unique_ptr<Result>> results);
 };
