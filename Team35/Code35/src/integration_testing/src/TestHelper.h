624169f2ea99a3876a3e58c0b9f28c829311213e
pangyuanker <e0660595@u.nus.edu>
2023-04-06 00:10:26 +0800

    Add integration tests

--- Team35/Code35/src/integration_testing/src/TestHelper.h
+++ Team35/Code35/src/integration_testing/src/TestHelper.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <functional>
+#include <list>
+#include <vector>
 
 #include "catch.hpp"
 
@@ -33,4 +35,34 @@ inline void requireEqualRef(const T &a, const T &b) {
 template<typename T>
 inline void requireNotEqual(T a, T b) {
     REQUIRE(a != b);
+}
+
+/**
+ * [1,2,2,2,2]
+ * [1,1,1,2,2]
+ *
+ * @tparam T
+ * @param a
+ * @param b
+ */
+template<typename T>
+inline void requireSameListNoOrder(const std::list<T> &a, const std::list<T> &b) {
+    requireEqual(a.size(), b.size());
+    for (auto &a_element : a) {
+        requireTrue(std::find(b.begin(), b.end(), a_element) != b.end());
+    }
+    for (auto &b_element : b) {
+        requireTrue(std::find(a.begin(), a.end(), b_element) != a.end());
+    }
+}
+
+template<typename T>
+inline void requireSameVectorNoOrder(const std::vector<T> &a, const std::vector<T> &b) {
+    requireEqual(a.size(), b.size());
+    for (auto &a_element : a) {
+        requireTrue(std::find(b.begin(), b.end(), a_element) != b.end());
+    }
+    for (auto &b_element : b) {
+        requireTrue(std::find(a.begin(), a.end(), b_element) != a.end());
+    }
 }
