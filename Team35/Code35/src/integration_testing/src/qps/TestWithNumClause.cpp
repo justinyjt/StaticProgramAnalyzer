f3d4e25335f7e846f8c5300963c00f4e075b2cba
justinyjt <e0560296@u.nus.edu>
2023-04-05 23:42:48 +0800

    Improve with clause test coverage

--- Team35/Code35/src/integration_testing/src/qps/TestWithNumClause.cpp
+++ Team35/Code35/src/integration_testing/src/qps/TestWithNumClause.cpp
@@ -13,8 +13,10 @@ TEST_CASE("1. Test WithNumClause") {
     PKBReader pkbReader(pkb);
     PKBWriter *pkbWriterPtr = &pkbWriter;
     PKBReader *pkbReaderPtr = &pkbReader;
-    STMT_SET setA{1, 3};
+    STMT_SET setA{1, 2, 3};
     STMT_SET setB{2, 3};
+    ENT_SET setC{"1", "2"};
+    pkbWriterPtr->addEntities(Entity::Constant, setC);
     pkbWriterPtr->addStatements(StmtType::None, setA);
     pkbWriterPtr->addStatements(StmtType::Assign, setB);
 
@@ -37,11 +39,81 @@ TEST_CASE("1. Test WithNumClause") {
         requireTrue(expectedResult == *actualResult);
     }
 
+    SECTION("Test with const syn = num") {
+        std::unique_ptr<Synonym> arg1 = std::make_unique<Synonym>(Synonym::DesignEntity::CONSTANT, "c");
+        std::unique_ptr<PQLNumber> arg2 = std::make_unique<PQLNumber>("2");
+        WithNumClause withNumClause = WithNumClause(std::move(arg1), std::move(arg2));
+        STMT_SET s{2};
+        TableResult expectedResult("c", s);
+        auto actualResult = withNumClause.evaluate(pkbReaderPtr);
+        requireTrue(expectedResult == *actualResult);
+    }
+
+    SECTION("Test with num = const syn") {
+        std::unique_ptr<Synonym> arg2 = std::make_unique<Synonym>(Synonym::DesignEntity::CONSTANT, "c");
+        std::unique_ptr<PQLNumber> arg1 = std::make_unique<PQLNumber>("2");
+        WithNumClause withNumClause = WithNumClause(std::move(arg1), std::move(arg2));
+        STMT_SET s{2};
+        TableResult expectedResult("c", s);
+        auto actualResult = withNumClause.evaluate(pkbReaderPtr);
+        requireTrue(expectedResult == *actualResult);
+    }
+
+    SECTION("Test with const syn = syn") {
+        std::unique_ptr<Synonym> arg1 = std::make_unique<Synonym>(Synonym::DesignEntity::CONSTANT, "c");
+        std::unique_ptr<Synonym> arg2 = std::make_unique<Synonym>(Synonym::DesignEntity::STMT, "s");
+        WithNumClause withNumClause(std::move(arg1), std::move(arg2));
+        STMT_STMT_SET s{std::make_pair(1, 1), std::make_pair(2, 2)};
+        TableResult expectedResult("c", "s", s);
+        auto actualResult = withNumClause.evaluate(pkbReaderPtr);
+        requireTrue(expectedResult == *actualResult);
+    }
+
+    SECTION("Test with syn = const syn") {
+        std::unique_ptr<Synonym> arg2 = std::make_unique<Synonym>(Synonym::DesignEntity::CONSTANT, "c");
+        std::unique_ptr<Synonym> arg1 = std::make_unique<Synonym>(Synonym::DesignEntity::STMT, "s");
+        WithNumClause withNumClause(std::move(arg1), std::move(arg2));
+        STMT_STMT_SET s{std::make_pair(1, 1), std::make_pair(2, 2)};
+        TableResult expectedResult("c", "s", s);
+        auto actualResult = withNumClause.evaluate(pkbReaderPtr);
+        requireTrue(expectedResult == *actualResult);
+    }
+
+    SECTION("Test with same const syn") {
+        std::unique_ptr<Synonym> arg1 = std::make_unique<Synonym>(Synonym::DesignEntity::CONSTANT, "c1");
+        std::unique_ptr<Synonym> arg2 = std::make_unique<Synonym>(Synonym::DesignEntity::CONSTANT, "c2");
+        WithNumClause withNumClause(std::move(arg1), std::move(arg2));
+        STMT_STMT_SET s{std::make_pair(1, 1), std::make_pair(2, 2)};
+        TableResult expectedResult("c1", "c2", s);
+        auto actualResult = withNumClause.evaluate(pkbReaderPtr);
+        requireTrue(expectedResult == *actualResult);
+    }
+
+    SECTION("Test with same non const syn") {
+        std::unique_ptr<Synonym> arg1 = std::make_unique<Synonym>(Synonym::DesignEntity::ASSIGN, "a");
+        std::unique_ptr<Synonym> arg2 = std::make_unique<Synonym>(Synonym::DesignEntity::ASSIGN, "a");
+        WithNumClause withNumClause(std::move(arg1), std::move(arg2));
+        STMT_SET s{2, 3};
+        TableResult expectedResult("a", s);
+        auto actualResult = withNumClause.evaluate(pkbReaderPtr);
+        requireTrue(expectedResult == *actualResult);
+    }
+
+    SECTION("Test with const syn = const syn") {
+        std::unique_ptr<Synonym> arg1 = std::make_unique<Synonym>(Synonym::DesignEntity::CONSTANT, "c");
+        std::unique_ptr<Synonym> arg2 = std::make_unique<Synonym>(Synonym::DesignEntity::CONSTANT, "c");
+        WithNumClause withNumClause(std::move(arg1), std::move(arg2));
+        STMT_SET s{1, 2};
+        TableResult expectedResult("c", s);
+        auto actualResult = withNumClause.evaluate(pkbReaderPtr);
+        requireTrue(expectedResult == *actualResult);
+    }
+
     SECTION("Test with syn = syn") {
         std::unique_ptr<Synonym> arg1 = std::make_unique<Synonym>(Synonym::DesignEntity::ASSIGN, "a");
         std::unique_ptr<Synonym> arg2 = std::make_unique<Synonym>(Synonym::DesignEntity::STMT, "s");
         WithNumClause withNumClause(std::move(arg1), std::move(arg2));
-        STMT_STMT_SET s{std::make_pair(3, 3)};
+        STMT_STMT_SET s{std::make_pair(3, 3), std::make_pair(2, 2)};
         TableResult expectedResult("a", "s", s);
         auto actualResult = withNumClause.evaluate(pkbReaderPtr);
         requireTrue(expectedResult == *actualResult);
